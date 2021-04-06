7b26fed0f009100f2be0fd3e369e86a58f23a334
yos10 <61726023+yos10@users.noreply.github.com>
2021-04-06 15:20:59 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,12 +7,14 @@
 */
 function factorial(n) {
   let result = 1;
-  // TODO このコメントを消して正しく実装してください。
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
   return result;
 }
 const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
-assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
-assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
-assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
+assert.strictEqual(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+assert.strictEqual(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
+assert.strictEqual(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+assert.strictEqual(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 console.log('すべてのテストを通過しました');
